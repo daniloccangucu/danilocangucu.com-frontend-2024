3f964e957396b895e9cf38e9852ee4d8a335fc74
Danilo Canguçu <daniloccangucu@gmail.com>
2024-08-14 15:57:55 +0200

    refactoring css

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import NavBar from './components/NavBar'
 import Home from './components/Home/Home'
+import BackgroundVideo from './components/Videos/BackgroundVideo';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <NavBar />
       <Home />
-    </div>
+      <BackgroundVideo />
+    </>
   );
 }
 
